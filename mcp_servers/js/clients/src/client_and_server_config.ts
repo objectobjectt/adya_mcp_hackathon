178b94bce3bbc503aa2c9e2d23975c2a3d1c2cbc
vivek patil <17vivekpatil@gmail.com>
2025-07-07 10:34:10 +0530

    added fedora client config

--- mcp_servers/js/clients/src/client_and_server_config.ts
+++ mcp_servers/js/clients/src/client_and_server_config.ts
@@ -1,25 +1,30 @@
-export const ClientsConfig:any = [
-    "MCP_CLIENT_OPENAI",
-    "MCP_CLIENT_AZURE_AI",
-    "MCP_CLIENT_GEMINI",
-    // "CLAUDE",
+export const ClientsConfig: any = [
+  "MCP_CLIENT_OPENAI",
+  "MCP_CLIENT_AZURE_AI",
+  "MCP_CLIENT_GEMINI",
+  // "CLAUDE",
 ]
 
-export const ServersConfig:any = [
-    // {
-    //     server_name :"WORDPRESS",
-    //     server_features_and_capability:`WORDPRESS`,
-    //     path : "build/index.js"
-    // },
-    {
-        server_name :"INFLUXDB",
-        server_features_and_capability:`INFLUXDB`,
-        path : "build/index.js"
-    },
-    {
-        server_name :"PODMAN",
-        server_features_and_capability:`PODMAN`,
-        path : "build/index.js"
-    }
+export const ServersConfig: any = [
+  // {
+  //     server_name :"WORDPRESS",
+  //     server_features_and_capability:`WORDPRESS`,
+  //     path : "build/index.js"
+  // },
+  {
+    server_name: "INFLUXDB",
+    server_features_and_capability: `INFLUXDB`,
+    path: "build/index.js"
+  },
+  {
+    server_name: "PODMAN",
+    server_features_and_capability: `PODMAN`,
+    path: "build/index.js"
+  },
+  {
+    server_name: "FEDORA",
+    server_features_and_capability: `FEDORA`,
+    path: "build/index.js"
+  },
 ]
 
